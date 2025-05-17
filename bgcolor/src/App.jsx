63d95bfeb1d9966542c88e0fde3772a4f13e1218
Usama Khan <umsiweb1234@gmail>
2025-05-17 12:16:26 +0500

    React Three Drei

--- bgcolor/src/App.jsx
+++ bgcolor/src/App.jsx
@@ -2,6 +2,12 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { Canvas } from '@react-three/fiber'
+import "./style.css"
+import React from 'react'
+import { OrbitControls } from '@react-three/drei'
+import Cyl from './Componants/Cyl'
+import { Bloom, EffectComposer, ToneMapping } from '@react-three/postprocessing'
 
 function App() {
 const [color, setColor] = useState("olive")
@@ -9,12 +15,30 @@ const [name, setName] = useState("Ummsi")
 
 return (
 <>
+
 <div className='w-full h-screen duration-200'
 style={{backgroundColor: color}}
 >
+
+      <Canvas flat camera={{fov:25}}>
+      <OrbitControls />
+      <ambientLight intensity={1.5} />
+    <Cyl />
+    <EffectComposer>
+    <Bloom
+    mipmapBlur // Use mipmap blur instead of Gaussian blur
+    intensity={12.0} // The bloom intensity.
+    luminanceThreshold={0} // luminance threshold. Raise this value to mask out darker elements in the scene.
+    luminanceSmoothing={1} // smoothness of the luminance threshold. Range is [0, 1]
+  />
+  <ToneMapping adaptive/>
+</EffectComposer>
+
+    </Canvas>
+
   <div className='flex flex-wrap justify-center items-center px-3 py-5'>
 
-    <div className='flex flex-wrap items-center justify-center gap-3 shadow-lg bg-white shadow-xl rounded-lg px-3 py-3'>
+<div className='flex flex-wrap items-center justify-center gap-3 bg-white shadow-xl rounded-lg px-3 py-3'>
       <button
       onClick={() =>setColor("maroon")}
       className='outline-none px-4 py-2 rounded-lg text-white font-semibold'
@@ -55,10 +79,10 @@ style={{backgroundColor: color}}
       className='outline-none px-4 py-2 rounded-lg text-white font-semibold'
       style={{backgroundColor:"Pink"}}
       >Pink</button>
-
     </div>
 
   </div>
+    <div className='flex justify-center items-center text-7xl font-bold mt-30'>Ummsi</div>
   </div> 
 
 </>
---
+++ bgcolor/src/Componants/Cyl.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import { useTexture } from '@react-three/drei'
+import * as THREE from 'three'
+import { useFrame } from '@react-three/fiber'
+import { useRef } from 'react'
+
+const Cyl = () => {
+    let tex = useTexture("./bg-7.png")
+    let cyl = useRef(null);
+    useFrame((state, delta )=> {
+    cyl.current.rotation.y += delta;
+    });
+return (
+<group rotation={[0,1,0.4]}>
+    <mesh ref={cyl} >
+    <cylinderGeometry args={[1, 1, 1, 60, 60, true]}/>
+    <meshStandardMaterial map={tex } transparent side={THREE.DoubleSide} />
+    </mesh>
+    </group>
+)
+}
+
+export default Cyl
